8aa2abd5b3fe134bb0804ea4ed0f0fdd511a6ef3
Ion Beristain <jonboyka@gmail.com>
2026-09-26 14:00:44 +0200

    POO aktoreen karga metodo batera eraman

--- ObjektuetaraOrientatutakoProgramazioa_Actores/src/Main.java
+++ ObjektuetaraOrientatutakoProgramazioa_Actores/src/Main.java
@@ -8,6 +8,16 @@ public class Main {
         // Aktore guztiak gordetzeko ArrayList-a
         ArrayList<Actor> actores = new ArrayList<>();
 
+        // Hasierako aktoreak ArrayList-ean kargatzen ditu
+        aktoreakKargatu(actores);
+
+        // Lehen aktorea erakusteko behin-behineko proba
+        System.out.println(actores.get(0));
+    }
+
+    // Hasierako aktoreen datuak ArrayList-ean kargatzen ditu
+    static void aktoreakKargatu(ArrayList<Actor> actores) {
+
         actores.add(new Actor(
                 1,
                 "Kevin Costner",
@@ -18,7 +28,5 @@ public class Main {
                 null,
                 true
         ));
-        
-        System.out.println(actores.get(0));
     }
 }
